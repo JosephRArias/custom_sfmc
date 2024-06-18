2db9d37cf08b585fff6c9046c549737995d05239
JosephRArias <joseam2969@gmail.com>
2024-06-18 15:58:42 -0400

    Small update

--- routes/customActivity.js
+++ routes/customActivity.js
@@ -1,31 +1,38 @@
-let postmonger = require('postmonger');
-let axios = require('axios');
+import postmonger from "postmonger";
+let axios = require("axios");
 
 var connection = new postmonger.Session();
 
-//window.ready(onRender);
-connection.trigger('ready');
-connection.on('initActivity', function(data){
-    document.getElementById('configuration').value = JSON.stringify(data, null, 2);
-    retrieveToken();
+document.addEventListener("DOMContentLoaded", function () {
+  connection.trigger("ready");
 });
 
-function retrieveToken () {
-    axios.post(tokenURL, { // Retrieving of token
+connection.on("initActivity", function (data) {
+  document.getElementById("configuration").value = JSON.stringify(
+    data,
+    null,
+    2
+  );
+  retrieveToken();
+});
+function retrieveToken() {
+  axios
+    .post(tokenURL, {
+      // Retrieving of token
 
-        username: 'emilys',
-        password: 'emilyspass'
-
+      username: "emilys",
+      password: "emilyspass",
     })
     .then(function (response) {
-        console.log('Auth');
-        console.log(response);
-        return response.data['access_token'];
-    }).catch(function (error) {
-        return error;
+      console.log("Auth");
+      console.log(response);
+      return response.data["access_token"];
+    })
+    .catch(function (error) {
+      return error;
     });
 }
 
-function onRender(){
-    connection.trigger('ready');
+function onRender() {
+  connection.trigger("ready");
 }
