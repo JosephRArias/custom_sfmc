ed75346f7dad39fc7ace04e2ba793d6618b45332
JosephRArias <joseam2969@gmail.com>
2024-06-18 16:19:29 -0400

    Trying another method for Postmonger

--- routes/customActivity.js
+++ routes/customActivity.js
@@ -1,4 +1,39 @@
-let postmonger = require('postmonger');
+define(["postmonger"], function (Postmonger) {
+    "use strict";
+  
+    var connection = new Postmonger.Session();
+    var payload = {};
+  
+    $(window).ready(onRender);
+  
+    connection.on('initActivity', function(data){
+        document.getElementById('configuration').value = JSON.stringify(data, null, 2);
+        retrieveToken();
+    });
+  
+
+    function onRender(){
+        connection.trigger('ready');
+    }
+    
+    function retrieveToken () {
+        axios.post(tokenURL, { // Retrieving of token
+    
+            username: 'emilys',
+            password: 'emilyspass'
+    
+        })
+        .then(function (response) {
+            console.log('Auth');
+            console.log(response);
+            return response.data['access_token'];
+        }).catch(function (error) {
+            return error;
+        });
+    }
+  });
+
+/*let postmonger = require('postmonger');
 let axios = require('axios');
 
 var connection = new postmonger.Session();
@@ -29,4 +64,4 @@ function retrieveToken () {
 
 function onRender(){
     connection.trigger('ready');
-}
+}*/
