03c56f6f7fb850cdb7871cbb7ee47de1ee243e87
JosephRArias <joseam2969@gmail.com>
2024-06-20 15:34:44 -0400

    Added method to getInArguments

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-let axios = require('axios');
+const querystring = require("querystring");
+let axios = require("axios");
 
 var indexRouter = require("./routes/index");
-var tokenURL = 'https://dummyjson.com/auth/login';
+var tokenURL = "https://dummyjson.com/auth/login";
+
+var request;
 
 var app = express();
 
@@ -17,19 +19,21 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 
 app.post("/execute", (req, res) => {
-
-    retrieveToken();
+  request = req.body;
+  retrieveToken();
+  console.log('Pase el token');
+  getInArgument('FirstName');
 });
-app.post("/save", function(req, res) {;
-    return res.status(200).json({});
-});
-
-app.post("/validate", function(req, res) {
-    return res.status(200).json({});
+app.post("/save", function (req, res) {
+  return res.status(200).json({});
 });
 
-app.post("/publish", function(req, res) {
-    return res.status(200).json({});
+app.post("/validate", function (req, res) {
+  return res.status(200).json({});
+});
+
+app.post("/publish", function (req, res) {
+  return res.status(200).json({});
 });
 
 
@@ -42,13 +46,23 @@ function retrieveToken() {
       password: "emilyspass",
     })
     .then(function (response) {
-      console.log("Auth");
-      console.log(response.data);
       return response.data["access_token"];
     })
     .catch(function (error) {
       return error;
     });
 }
-
+function getInArgument(k) {
+  if (request && request.inArguments) {
+      for (let i = 0; i < request.inArguments.length; i++) {
+          let e = request.inArguments[i];
+          if (k in e) {
+            console.log(e[k]);
+              return e[k];
+          }
+      }
+  }
+  console.log("Unable To Find In Argument: ", k);
+  return;
+}
 module.exports = app;
